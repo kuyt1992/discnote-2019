f502ee5d6fc00f56e772e74a7902306ffed742d6
kaito.tada <kuyt1992@gmail.com>
2019-09-01 09:39:06 +0000

    for heroku3

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Artists.create(name: 'Spitz')
-Albums.create(title:'三日月ロック', artist_name:'Spitz')
+Artist.create(name: 'Spitz')
+Album.create(title:'三日月ロック', artist_name:'Spitz')

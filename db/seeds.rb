973fed8bc6841125b4796a673048bd1de83d0e83
kaito.tada <kuyt1992@gmail.com>
2019-09-17 15:34:54 +0000

    albums

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,84 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Artist.create(
     name: 'Spitz',
-    #image: open("#{Rails.root}/db/fixtures/spitz.jpg")
+    image: open("#{Rails.root}/db/fixtures/spitz.jpg")
 )
 
-Artist.first.update_attributes(image: File.open("#{Rails.root}/db/fixtures/spitz.jpg"))
+#Artist.first.update_attributes(image: File.open("#{Rails.root}/db/fixtures/spitz.jpg"))
 
 
 Album.create(
     title:'三日月ロック', 
     artist_id:1,
     image: open("#{Rails.root}/db/fixtures/mikaduki.jpg")
+)
+
+Album.create(
+    title:'ハチミツ', 
+    artist_id:1,
+    image: open("#{Rails.root}/db/fixtures/hachimitsu.jpg")
+)
+
+Album.create(
+    title:'フェイクファー', 
+    artist_id:1,
+    image: open("#{Rails.root}/db/fixtures/fakefur.jpg")
+)
+
+Album.create(
+    title:'インディゴ地平線', 
+    artist_id:1,
+    image: open("#{Rails.root}/db/fixtures/indigo.jpg")
+)
+
+Artist.create(
+    name: 'Death Cab for Cutie',
+    image: open("#{Rails.root}/db/fixtures/dcfc.jpg")
+)
+
+Album.create(
+    title:"We Have the Facts and We're Voting Yes", 
+    artist_id:2,
+    image: open("#{Rails.root}/db/fixtures/wehavethefacts.jpg")
+)
+
+Album.create(
+    title:'The Photo Album', 
+    artist_id:2,
+    image: open("#{Rails.root}/db/fixtures/photoalbum.jpg")
+)
+
+Album.create(
+    title:'Transatlanticism', 
+    artist_id:2,
+    image: open("#{Rails.root}/db/fixtures/transatlanticism.jpg")
+)
+
+Album.create(
+    title:'Plans', 
+    artist_id:2,
+    image: open("#{Rails.root}/db/fixtures/plans.jpg")
+)
+
+Artist.create(
+    name: 'Radiohead',
+    image: open("#{Rails.root}/db/fixtures/radiohead.jpg")
+)
+
+Album.create(
+    title:'The Bends', 
+    artist_id:3,
+    image: open("#{Rails.root}/db/fixtures/bends.jpg")
+)
+
+Album.create(
+    title:'OK Computer', 
+    artist_id:3,
+    image: open("#{Rails.root}/db/fixtures/okcomputer.jpg")
+)
+
+Album.create(
+    title:'Kid A', 
+    artist_id:3,
+    image: open("#{Rails.root}/db/fixtures/kida.jpg")
 )
